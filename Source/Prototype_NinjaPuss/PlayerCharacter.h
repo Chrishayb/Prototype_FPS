de762febb6c92928ab73ddd619f61fc41b3d435e
Enderderder <richardwulansari@gmail.com>
2019-02-20 01:37:54 +1300

    ~ Setup the basic movement for player character

--- Source/Prototype_NinjaPuss/PlayerCharacter.h
+++ Source/Prototype_NinjaPuss/PlayerCharacter.h
@@ -6,21 +6,85 @@
 #include "GameFramework/Character.h"
 #include "PlayerCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class EDirection : uint8
+{
+	LEFT,
+	RIGHT,
+	FORWARD,
+	BACKWARD
+};
+
 UCLASS()
 class PROTOTYPE_NINJAPUSS_API APlayerCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
+#pragma region CameraComponents
+
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+
+#pragma endregion CameraComponents
+	
 public:
 	// Sets default values for this character's properties
 	APlayerCharacter();
 
+#pragma region CameraStats
+
+	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate;
+
+	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate;
+
+#pragma endregion CameraStats
+
+#pragma region DodgeStats
+
+
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Combat")
-	int DodgeCooldown;
+	float DodgeCooldown;
+
+
+
+#pragma endregion DodgeStats
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	/** Called for forwards/backward input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
+
+	/**
+	 * Called via input to turn at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void TurnAtRate(float Rate);
+
+	/**
+	 * Called via input to turn look up/down at a given rate.
+	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
+	 */
+	void LookUpAtRate(float Rate);
+
+	/** Called for character dash/dodge ability */
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	void Dash();
 
 public:	
 	// Called every frame
